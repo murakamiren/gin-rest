e1f1b0c3f0d7801358c921989e81eb38b93f3c7a
renmurakami <autoeyy@gmail.com>
2022-07-06 11:44:15 +0900

    init

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"gin-rest/src"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	src.DefineRoutes(r)
+	r.Run(":5000")
+}
---
+++ src/routes.go
@@ -0,0 +1,22 @@
+package src
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func testGetV1() gin.HandlerFunc {
+	return func (c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "v1 api test",
+		})
+	}
+}
+
+func DefineRoutes(router gin.IRouter) {
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/test", testGetV1())
+	}
+}
